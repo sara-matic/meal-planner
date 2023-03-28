e94c687d0ee8351535fcaaa78bf10e50cdd27abc
sara-matic <smatic.97@gmail.com>
2023-03-28 19:05:34 +0200

    Link Building

--- meal_planner/meals/urls.py
+++ meal_planner/meals/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('breakfast/<int:id>', views.breakfast),
-    path('lunch/<int:id>', views.lunch),
-    path('dinner/<int:id>', views.dinner)
+    path('breakfast/<int:id>', views.breakfast, name='breakfast'),
+    path('lunch/<int:id>', views.lunch, name='lunch'),
+    path('dinner/<int:id>', views.dinner, name='dinner')
 ]
