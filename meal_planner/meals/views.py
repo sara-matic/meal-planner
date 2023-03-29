5c752bdc3bf3e034ac031d37d33c6a6f3a4a8678
sara-matic <smatic.97@gmail.com>
2023-03-29 19:36:19 +0200

    Add Edit and Delete options for Lunch, Dinner, Food Add new pages for each Food instance

--- meal_planner/meals/views.py
+++ meal_planner/meals/views.py
@@ -61,6 +61,23 @@ def new_lunch(request):
         form = LunchForm
     return render(request, "meals/new.html", {"form": form, "meal": "Lunch"})
 
+def edit_lunch(request, id):
+    lunch = get_object_or_404(Lunch, pk=id)
+    if request.method == "POST":
+        form = LunchForm(request.POST, instance=lunch)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = LunchForm(instance=lunch)
+    return render(request, "meals/edit.html", {"form": form, "meal": "Lunch"})
+
+def delete_lunch(request, id):
+    lunch = get_object_or_404(Lunch, pk=id)
+    if request.method == "POST":
+        lunch.delete()
+        return redirect("welcome")
+    return render(request, "meals/confirm.html", {"meal_name": "Lunch", "meal": lunch, "view_function_name": "lunch"})
 
 def new_dinner(request):
     if request.method == "POST":
@@ -73,10 +90,27 @@ def new_dinner(request):
         form = DinnerForm
     return render(request, "meals/new.html", {"form": form, "meal": "Dinner"})
 
+def edit_dinner(request, id):
+    dinner = get_object_or_404(Dinner, pk=id)
+    if request.method == "POST":
+        form = DinnerForm(request.POST, instance=dinner)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = DinnerForm(instance=dinner)
+    return render(request, "meals/edit.html", {"form": form, "meal": "Dinner"})
+
+def delete_dinner(request, id):
+    dinner = get_object_or_404(Dinner, pk=id)
+    if request.method == "POST":
+        dinner.delete()
+        return redirect("welcome")
+    return render(request, "meals/confirm.html", {"meal_name": "Dinner", "meal": dinner, "view_function_name": "dinner"})
 
 def all_food(request):
     return render(request, "meals/food.html",
-                  {"food": Food.objects.order_by('name')})
+                  {"all_food": Food.objects.order_by('name')})
 
 def new_food(request):
     if request.method == "POST":
@@ -84,9 +118,30 @@ def new_food(request):
         form = FoodForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("food")
-
+            return redirect("all_food")
     else:
         form = FoodForm
     return render(request, "meals/new.html", {"form": form, "meal": "Food"})
 
+def food(request, id):
+    meal = get_object_or_404(Food, pk=id)
+    return render(request, "meals/food_by_id.html",
+                  {"food": meal, "food_name": meal.name})
+
+def edit_food(request, id):
+    food = get_object_or_404(Food, pk=id)
+    if request.method == "POST":
+        form = FoodForm(request.POST, instance=food)
+        if form.is_valid():
+            form.save()
+            return redirect("all_food")
+    else:
+        form = FoodForm(instance=food)
+    return render(request, "meals/edit.html", {"form": form, "meal": "Food"})
+
+def delete_food(request, id):
+    food = get_object_or_404(Food, pk=id)
+    if request.method == "POST":
+        food.delete()
+        return redirect("all_food")
+    return render(request, "meals/confirm.html", {"meal_name": "Food", "meal": food, "view_function_name": "food"})
